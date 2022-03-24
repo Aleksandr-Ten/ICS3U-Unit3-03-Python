6fffdde7eeeff2d72ed11cd9ee453d0d4cc0d365
Aleksandr Ten <aleksandr.ten@mths.ca>
2022-03-24 18:50:49 +0000

    added python

--- number_game.py
+++ number_game.py
@@ -4,19 +4,22 @@
 # Created on: March 2022
 # This program tells if the number user inputs is correct
 
-import constants
+import random
 
 
 def main():
     # this function checks if the number user guessed is correct
 
-    # tnput
+    # input
+    random_number = random.randint(0, 9)
     guessing_number = int(input("Enter the guessing number:"))
     print("")
 
     # process and output
-    if guessing_number == constants.CORRECT_NUMBER:
+    if guessing_number == random_number:
         print("Correct!")
+    else:
+        print("Incorrect!")
 
 
 if __name__ == "__main__":
